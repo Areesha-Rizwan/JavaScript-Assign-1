0ccba42c6a69851e77406efe5bea5080faa92a87
Areesha <133148078+Areesha-Rizwan@users.noreply.github.com>
2024-04-28 00:23:00 +0500

    Add files via upload

---
+++ JS ass1/task1/script.js
@@ -0,0 +1,23 @@
+// task1
+
+alert("Hey! Have a great day.")
+
+// task2
+
+alert("Error! Please write a valid password.")
+
+// task3
+
+alert("welcome to JS Land....\nHappy Coding!");
+
+// task4
+
+alert("Welcome to JS land...")
+
+// task5
+
+alert("Happy Coding!\nPrevent this page from adding additional dialogs")
+
+// task6
+
+alert("Hello...I can run JS through my web browser's console")
