54e12df76138a5f290b55162d15bfce108ece0e8
Areesha <133148078+Areesha-Rizwan@users.noreply.github.com>
2024-05-08 00:35:25 +0500

    Add files via upload

---
+++ task2/script.js
@@ -0,0 +1,14 @@
+var age = 17; 
+alert("I am 17 years old"+ age);
+
+
+
+var customerVisit = ('11');
+
+// If it's not stored, initialize it to 0
+if ( customerVisit=== 11) {
+    visitCount = 11;
+}
+
+
+alert("You have visited this site 11 times " + "customerVisit");
