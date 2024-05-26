df67d180672f004f25a3066a42233dde78f26021
Areesha <133148078+Areesha-Rizwan@users.noreply.github.com>
2024-05-26 11:13:37 -1200

    Add files via upload

---
+++ task4/script.js
@@ -0,0 +1,18 @@
+// Declare an empty array using JS literal notation to store
+// student names in future.
+
+var studentNames = ();
+
+// Declare an empty array using JS object notation to store
+// student names in future.
+
+var studentNames = {};
+
+// Declare and initialize a strings array.
+
+
+var main() {
+    strArray := []string{"Hello", "World", "Go", "Programming"}
+    console.log main (strArray)
+}
+
